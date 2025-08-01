6ed8fd629b8da6155ed2c7340987fcca950463e7
Firdous Quadri <firdousquadri55@gmail.com>
2025-08-01 18:34:11 +0530

    Update Pattern 20.cpp
    updated

--- Pattern 20.cpp
+++ Pattern 20.cpp
@@ -70,6 +70,47 @@ int main(){
 }
 
 
+// Striver's way
+
+int main(){
+    
+     int n;
+     cout<<"Enter number: ";
+     cin>>n;
+    
+    int spaces=2*n-2;
+    
+    for(int i=1; i<=2*n-1; i++){
+        int stars=i;
+        if(i>n){
+         stars =2*n-i;   
+        } 
+        
+        // for stars
+        for(int j=1; j<=stars; j++){
+            cout<<"*";
+        }
+        
+        // for spaces
+        for(int j=1; j<=spaces; j++){
+            cout<<" ";
+        }
+        
+        // for stars
+        for(int j=1; j<=stars; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+        if(i<n){
+            spaces=spaces-2;
+        }
+        else{
+            spaces=spaces+2;
+        }
+        
+    }
+    
+
 
 
 
