28d18065a47b7a3dbeeceb782063b22418ffb80b
Firdous Quadri <firdousquadri55@gmail.com>
2025-08-05 10:29:15 +0530

    Add files via upload

---
+++ Pattern23.cpp
@@ -0,0 +1,42 @@
+/*WAP to print 
+
+4 4 4 4 4 4 4
+4 3 3 3 3 3 4
+4 3 2 2 2 3 4
+4 3 2 1 2 3 4
+4 3 2 2 2 3 4
+4 3 3 3 3 3 4
+4 4 4 4 4 4 4     */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+     int n;
+     cout<<"Enter number: ";
+     cin>>n;
+    
+    
+    for(int i=0; i<2*n-1; i++){
+        for(int j=0; j<2*n-1; j++){
+            int top=i;
+            int left=j;
+            int right=(2*n-2)-j; /*can also write as (2*n-1)-1-j*/
+            int bottom=(2*n-2)-i;
+            cout<<(n-min(min(top, bottom), min(left, right)))<<" ";
+        }
+        cout<<endl;
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
